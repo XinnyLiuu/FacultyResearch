9b72158b77f787fe981194459e4d1a2f548fa319
Xin Liu <xhliu97@gmail.com>
2019-03-24 18:24:19 -0400

    finished demo

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
-const mysql = require('./mysql');
+'use strict';
+const mysql = require('./db/mysql');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const express = require('express');
+const morgan = require('morgan');
+const http = require('http');
+const path = require('path');
 
-// DB connection
-mysql.connect();
+// Controllers (route handlers)
+const equipmentController = require("./controllers/equipmentController");
 
-// Sample insert
-// id 1, name test, desc test, cap 1
-let values = [ 1, "test", "test", 1 ];
-mysql.insert("insert into equipment values(?, ?, ?, ?)", values);
+// Create express server
+const app = express();
 
-// Sample update
-values = [ "NEW NAME", 1 ];
-mysql.update("update equipment set equipmentname = ? where equipid = ?", values);
+// Set up Handlebars templating
+const hbs = exphbs.create({
+    defaultLayout: "layout.hbs"
+}); 
+app.engine('hbs', hbs.engine);
+app.set("view engine", "hbs"); 
 
-// Sample query using select
-mysql.select("select * from equipment");
+// Server configs
+app.use(bodyParser.json()); // Support application/json post data
+app.use(bodyParser.urlencoded({ extended: true })); // Support application/x-www-form-urlencoded post data
+app.use(express.static(
+    path.join(__dirname, 'public')
+));
 
-// Sample delete
-// id 1
-values = [ 1 ];
-mysql.remove("delete from equipment where equipid = ?", values);
+// Terminal output layout
+app.use(morgan(':date[iso] :status :method :url [:response-time ms] :remote-addr')); 
 
-mysql.close();
+/**
+ * Routes
+ */
+app.get("/", equipmentController.getAllEquipment);
+
+// Connect to DB and start Express server
+mysql.connect()
+.then(success => {
+    console.log( success );
+
+    // Start Express server
+    const httpServer = http.createServer( app );
+    const httpPort = 8000;
+    httpServer.listen( httpPort );
+    console.log(`Express server listening on port ${httpPort}`);
+})
+.catch(err => {
+    console.log(err);
+    process.exit( 1 );
+});
