5703257bfc23721b2d0324b79aee0bac7d29c81d
Xin Liu <xhliu97@gmail.com>
2019-04-10 15:28:11 -0400

    added test data for all tables

--- faculty_research.sql
+++ faculty_research.sql
@@ -53,49 +53,52 @@ CREATE TABLE student(
     FOREIGN KEY(DepartmentID) REFERENCES department(DepartmentID)
 );
 
-
-
 --
 -- Dumping data for table 'faculty'
 --
-
 -- Fields: FacultyID, DepartmentID, FirstName, LastName,Email,Interest,OfficeBuilding,OfficePhoneNumber,OfficeRoomNumber
--- CS - 21
 INSERT INTO faculty VALUES(11, 21, "Reynold", "Bailey", "rjb@cs.rit.edu", "CS", "Golisano", "585-475-6181", "GOL-3005");
 INSERT INTO faculty VALUES(12, 21, "Ivona", "Bezakova", "ib@cs.rit.edu", "CS", "Golisano", "585-475-4526", "GOL-3645");
-
--- CSEC - 22
 INSERT INTO faculty VALUES(13, 22, "Giovani", "Abuaitah", "graics@rit.edu", "CSEC", "Golisano", "585-475-4316", "GOL-2321");
 INSERT INTO faculty VALUES(14, 22, "Hrishikesh", "Archarya", "hbaics@rit.edu", "CSEC", "Golisano", "585-475-2801", "GOL-2647");
-
--- IST - 23
 INSERT INTO faculty VALUES(15, 23, "Dan", "Bogaard", "dsbics@rit.edu", "ISTE", "Golisano", "585-475-5231", "GOL-2111");
 INSERT INTO faculty VALUES(16, 23, "Michael", "Floeser", "Michael.Floeser@rit.edu", "ISTE", "Golisano", "585-475-7031", "GOL-2669");
-
--- IGM - 24
 INSERT INTO faculty VALUES(17, 24, "David", "Schwartz", "disvks@rit.edu", "IGM", "Golisano", "585-475-5521", "GOL-2157");
 INSERT INTO faculty VALUES(18, 24, "Jessica", "Bayliss", "jdbics@rit.edu", "IGM", "Golisano", "585-475-2507", "GOL-2153");
-
--- SE - 25
 INSERT INTO faculty VALUES(19, 25, "Travis", "Desell", "tjdvse@rit.edu", "SE", "Golisano", NULL, "GOL-1559");
 INSERT INTO faculty VALUES(110, 25, "Scott", "Hawker", "hawker@mail.rit.edu", "SE", "Golisano", "585-475-2705", "GOL-1696");	
-
 
 
 --
 -- Dumping data for table 'department'
 --
-
 -- Fields: DepartmentID, DepartmentName, OfficeNumber
-INSERT INTO department VALUES(); 
+INSERT INTO department VALUES(21, "Computer Science","GOL-3005"); 
+INSERT INTO department VALUES(22, "Computing Security","GOL-2120"); 
+INSERT INTO department VALUES(23, "Information Sciences & Technologies","GOL-2100"); 
+INSERT INTO department VALUES(24, "School of Interactive Games & Media","GOL-2145"); 
+INSERT INTO department VALUES(25, "Software Engineering","GOL-1690"); 
 
 
+--
+-- Dumping data for table 'student'
+--
+-- Fields: StudentID, DepartmentID, FirstName, LastName, Email, SchoolYear, Major
+INSERT INTO student VALUES(31, 21, 'John', 'Smith','js1434@rit.edu','Sophomore','CS');
+INSERT INTO student VALUES(32, 22, 'George', 'Brown','gb6745@rit.edu','Junior','CSEC');
+INSERT INTO student VALUES(33, 23, 'Carmen', 'Tang','ct7891@rit.edu','Freshman','IST');
+INSERT INTO student VALUES(34, 24, 'Laura', 'Diaze','ldz3142@rit.edu','Freshman','IGM');
+INSERT INTO student VALUES(35, 25, 'Fred', 'Amarty','fd5930@rit.edu','Junior','SE');
+INSERT INTO student VALUES(36, 21, 'Ronald', 'Sowah','rs9030@rit.edu','Senior','CS');
+INSERT INTO student VALUES(37, 22, '', 'Desley','Brown','db8930@rit.edu','Freshman','CSEC');
+INSERT INTO student VALUES(38, 23, 'Tiffany', 'Dubon','td8197@rit.edu','Sophomore','IST');
+INSERT INTO student VALUES(39, 24, 'Lauren', 'Campbell','lc2543@rit.edu','Freshman','IGM');
+INSERT INTO student VALUES(310, 25, 'Sam', 'Illesamni','smoi3030@rit.edu','Junior','SE');
 
 
 --
 -- Dumping data for table 'project'
 --
-
 -- Fields: ProjectID, FacultyID, ProjectName, ProjectDescription, Budget, StartDate, EndDate
 INSERT INTO project VALUES(41, 11, 'Lookup or Learn: A Grounded Theory Approach to the Student-Task', 'Proceedings of the 9th International Conference on Education and Information Systems, Technologies and Applications', '2018-08-27','2018-12-11');
 INSERT INTO project VALUES(42, 12, 'Progress on Website Accessibility?' , 'ACM Transactions on the Web','2017-08-22','2017-12-08');
@@ -107,14 +110,5 @@ INSERT INTO project VALUES(47,17,'Implementation and Evaluation of Animation Con
 INSERT INTO project VALUES(48,18,'Release of Experimental Stimuli and Questions for Evaluating Facial Expressions in Animations of American Sign Language',' Proceedings of the 6th Workshop on the Representation and Processing of Sign Languages: Beyond the Manual Channel, The 9th International Conference on Language Resources and Evaluation','2018-08-31','2018-12-02');
 INSERT INTO project VALUES(49,19, 'Using a Low-Cost Open Source Hardware Development Platform in Teaching Young Students Programming Skills','Proceedings of the SIGITE','2017-01-31','2017-05-04');
 INSERT INTO project VALUES(410,110,'Teaching Android Malware Behaviors for Android Platform Using Interactive Labs','Proceedings of the ASIA 2013','2017-08-25','2017-12-13');
-
-
-
---
--- Dumping data for table 'student'
---
-
--- Fields: StudentID, DepartmentID, FirstName, LastName, Email, SchoolYear, Major
-INSERT INTO student VALUES();
 
 
